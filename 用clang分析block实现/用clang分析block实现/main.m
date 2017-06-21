d32e8326de384517805f3a674400f0d0a6ee7814
yibuyuyimiao <1445341812@qq.com>
2017-06-21 16:54:54 +0800

    新建项目

---
+++ 用clang分析block实现/用clang分析block实现/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  用clang分析block实现
+//
+//  Created by ls on 2017/6/21.
+//  Copyright © 2017年 yibuyimiao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
